fa3c99a674bda845779596345b3360b16a9a43f0
Ianb529 <ian.beltran@ucsp.edu.pe>
2025-03-19 08:21:13 -0500

    Add files via upload

---
+++ suma 2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    float num2;
+    num1=4;
+    num2=2;
+    short num3=327659;
+    cout << (num1+num2) << endl;
+    cout << (num1-num2) << endl;
+    cout << (num1*num2) << endl;
+    cout << (num1/num2) << endl;
+    cout << "el tamaño del numero short es:" << sizeof(num3) << endl;
+}
