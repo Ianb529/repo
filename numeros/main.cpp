a052ccd43623dfe9efc616103ac6ea63457efec4
Ianb529 <ian.beltran@ucsp.edu.pe>
2025-03-26 08:24:10 -0500

    Add files via upload

---
+++ numeros/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    char num1;
+    char num2;
+
+    cout << "ingrese un caracter: ";
+    cin >> num1;
+    cout << "ingrese un caracter: ";
+    cin >> num2;
+    int valNum1 = static_cast<int>(num1);
+    int valNum2 = static_cast<int>(num2);
+    if( (valNum1 >= 48 && valNum1 <=57) && (valNum2 >= 48 && valNum2 <=57) ) {
+            cout <<"La suma es : " << ((valNum1-48) + (valNum2-48)) <<endl;
+    } else {
+        cout << "Solo numeros papu" << endl;
+    }
+    return 0;
+
+    cout << 'A' << endl;
+    cout << "A" << endl;
+
+    string name;
+    int age;
+    cout <<"ingresa tu nombre: ";
+    cin >> name;
+    cout <<"ingresa tu edad: ";
+    cin >> age;
+
+    cout <<"Hanla pes " << name << " que fue ps" << endl;
+    cout << "con que " << age << " años no papu?" << endl;
+    return 0;
+    */
+    string cadena = "Ciencia de la Computacion";
+
+    cout << cadena.at(0) << endl;
+    cout << cadena.at(1) << endl;
+    cout << cadena.at(2) << endl;
+    cout << cadena.at(3) << endl;
+    cout << cadena.at(4) << endl;
+    cout << cadena.at(5) << endl;
+    cout << cadena.at(6) << endl;
+    cout << cadena.at(7) << endl;
+    cout << cadena.at(8) << endl;
+    cout << cadena.at(9) << endl;
+    cout << cadena.at(10) << endl;
+    cout << cadena.at(11) << endl;
+    cout << cadena.at(12) << endl;
+    cout << cadena.at(13) << endl;
+    cout << cadena.at(14) << endl;
+    cout << cadena.at(15) << endl;
+    cout << cadena.at(16) << endl;
+    cout << cadena.at(17) << endl;
+    cout << cadena.at(18) << endl;
+    cout << cadena.at(19) << endl;
+    cout << cadena.at(20) << endl;
+    cout << cadena.at(21) << endl;
+    cout << cadena.at(22) << endl;
+    cout << cadena.at(23) << endl;
+    cout << cadena.at(24) << endl;
+    cout << cadena.at(25) << endl;
+
+}
